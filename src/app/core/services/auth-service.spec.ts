a14a48c3e7e910a272d145223f885eb156f3d995
Mbonisi Mpala <mpalambonisi63@gmail.com>
2025-10-05 17:44:13 +0200

    test(service): ensure valid user registration succeeds

--- src/app/core/services/auth-service.spec.ts
+++ src/app/core/services/auth-service.spec.ts
@@ -1,16 +1,43 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth-service';
+import { provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [AuthService, provideHttpClient(), provideHttpClientTesting()],
+    });
     service = TestBed.inject(AuthService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should register a user successfully', () => {
+    const mockUser = {
+      firstName: 'John',
+      lastName: 'Doe',
+      username: 'johndoe',
+      email: 'johndoe@yahoo.com',
+      password: 'StrongPassword1234',
+    };
+
+    const mockResponse = { message: 'User registered successfully!' };
+
+    service.register(mockUser).subscribe(response => {
+      expect(response).toEqual(mockResponse);
+    });
+
+    const req = httpMock.expectOne('http://3.71.52.212/auth/register');
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual(mockUser);
+    req.flush(mockResponse);
   });
 });
