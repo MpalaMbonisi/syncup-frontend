236fb16d47b7b229c5eb55c773f89ce3b176c4e8
Mbonisi Mpala <mpalambonisi63@gmail.com>
2025-10-05 17:47:14 +0200

    feat(auth-api): create register api method

--- src/app/core/services/auth-service.ts
+++ src/app/core/services/auth-service.ts
@@ -1,6 +1,27 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface RegisterRequest {
+  firstName: string;
+  lastName: string;
+  username: string;
+  email: string;
+  password: string;
+}
+
+export interface RegisterResponse {
+  message: string;
+}
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthService {}
+export class AuthService {
+  private apiUrl = 'http://3.71.52.212/auth';
+  private http: HttpClient = inject(HttpClient);
+
+  register(userData: RegisterRequest): Observable<RegisterResponse> {
+    return this.http.post<RegisterResponse>(`${this.apiUrl}/register`, userData);
+  }
+}
