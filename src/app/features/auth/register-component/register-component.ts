c770c0f32e3deefe242a6951fc06c09577f33023
Mbonisi Mpala <mpalambonisi63@gmail.com>
2025-10-05 16:56:20 +0200

    feat(auth): implement register form validation and states updates

--- src/app/features/auth/register-component/register-component.ts
+++ src/app/features/auth/register-component/register-component.ts
@@ -25,5 +25,10 @@ export class RegisterComponent {
     });
   }
 
-  onSubmit() {}
+  onSubmit() {
+    if (this.registerForm.valid) {
+      this.successMessage = '';
+      this.errorMessage = '';
+    }
+  }
 }
