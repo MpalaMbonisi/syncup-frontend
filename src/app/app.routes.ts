e59c45710ef9be6ef72d0582a0d4e9479fd54e57
Mbonisi Mpala <mpalambonisi63@gmail.com>
2025-10-08 16:53:05 +0200

    refactor(routes): remove unused login component route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,4 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './features/auth/register-component/register-component';
-import { LoginComponent } from './features/auth/login-component/login-component';
 
-export const routes: Routes = [
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-];
+export const routes: Routes = [{ path: 'register', component: RegisterComponent }];
