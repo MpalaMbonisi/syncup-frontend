2e4a77ddfbe02f895769953d2da2c47b53fadf6e
Mbonisi Mpala <mpalambonisi63@gmail.com>
2025-10-09 11:24:13 +0200

    feat(auth): implement login form markup and client-side validation

--- src/app/features/auth/login-component/login-component.ts
+++ src/app/features/auth/login-component/login-component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FooterComponent } from '../../../shared/components/footer-component/footer-component';
 
 @Component({
   selector: 'app-login-component',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, FooterComponent],
   templateUrl: './login-component.html',
   styleUrl: './login-component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  private formBuilder = inject(FormBuilder);
+
+  loginForm: FormGroup;
+  errorMessage: string = '';
+  showPassword: boolean = false;
+
+  constructor() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  onSubmit() {}
+}
