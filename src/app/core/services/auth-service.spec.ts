87618c29588f442c356b7fcff5f8b0b3c2792e19
Mbonisi Mpala <mpalambonisi63@gmail.com>
2025-10-12 23:54:44 +0200

    test(auth-service): verify successful user registration via api

--- src/app/core/services/auth-service.spec.ts
+++ src/app/core/services/auth-service.spec.ts
@@ -16,28 +16,32 @@ describe('AuthService', () => {
     httpMock = TestBed.inject(HttpTestingController);
   });
 
+  afterEach(() => {
+    httpMock.verify();
+  });
+
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should register a user successfully', () => {
-    const mockUser = {
-      firstName: 'John',
-      lastName: 'Doe',
-      username: 'johndoe',
-      email: 'johndoe@yahoo.com',
-      password: 'StrongPassword1234',
-    };
+  describe('register', () => {
+    it('should register a user successfully', () => {
+      const mockUser = {
+        firstName: 'John',
+        lastName: 'Doe',
+        username: 'johndoe',
+        email: 'johndoe@yahoo.com',
+        password: 'StrongPassword1234',
+      };
 
-    const mockResponse = { message: 'User registered successfully!' };
+      const mockResponse = { message: 'User registered successfully!' };
 
-    service.register(mockUser).subscribe(response => {
-      expect(response).toEqual(mockResponse);
+      const req = httpMock.expectOne('http://3.71.52.212/auth/register');
+      service.register(mockUser).subscribe(() => {
+        expect(req.request.method).toBe('POST');
+        expect(req.request.body).toEqual(mockUser);
+        req.flush(mockResponse);
+      });
     });
-
-    const req = httpMock.expectOne('http://3.71.52.212/auth/register');
-    expect(req.request.method).toBe('POST');
-    expect(req.request.body).toEqual(mockUser);
-    req.flush(mockResponse);
   });
 });
