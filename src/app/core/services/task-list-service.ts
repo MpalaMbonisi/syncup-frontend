886617885d789e480aa5579537f22ccb6b82601b
Mbonisi Mpala <mpalambonisi63@gmail.com>
2025-12-08 03:46:23 +0100

    feat(service): implement list retrieval method

---
+++ src/app/core/services/task-list-service.ts
@@ -0,0 +1,38 @@
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface TaskItemResponseDTO {
+  id: number;
+  description: string;
+  completed: boolean;
+  taskListTitle: string;
+}
+
+export interface TaskListResponseDTO {
+  id: number;
+  title: string;
+  owner: string;
+  collaborators: string[];
+  tasks: TaskItemResponseDTO[];
+}
+
+export interface TaskListCreateDTO {
+  title: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskListService {
+  private apiUrl = `${environment.apiUrl}/list`;
+  private http: HttpClient = inject(HttpClient);
+
+  /**
+   * Get all task lists for the current user (owned + collaborated)
+   */
+  getAllLists(): Observable<TaskListResponseDTO[]> {
+    return this.http.get<TaskListResponseDTO[]>(`${this.apiUrl}/all`);
+  }
+}
