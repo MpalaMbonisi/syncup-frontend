3ddf99f919344e170090df09d228a31d5dd2a540
Mbonisi Mpala <mpalambonisi63@gmail.com>
2026-03-15 16:27:45 +0100

    fix(deps): patch angular xss and jest jsdom vulnerabilities
    bump @angular/* to 20.3.18 (GHSA-g93w-mfhg-p222) and upgrade
    jest-preset-angular to 16.1.1 with jest 30 (GHSA-vpq2-c234-7xj6)

--- jest.config.js
+++ jest.config.js
@@ -1,36 +1,33 @@
 module.exports = {
   preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  testPathIgnorePatterns: [
-    '<rootDir>/node_modules/',
-    '<rootDir>/dist/'
-  ],
+  setupFilesAfterFramework: ['<rootDir>/setup-jest.ts'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
   coverageDirectory: 'coverage',
   coverageReporters: ['html', 'lcov', 'text-summary'],
   collectCoverageFrom: [
     'src/**/*.ts',
     '!src/**/*.spec.ts',
     '!src/main.ts',
-    '!src/environments/**'
+    '!src/environments/**',
   ],
   moduleNameMapper: {
     '^@app/(.*)$': '<rootDir>/src/app/$1',
     '^@core/(.*)$': '<rootDir>/src/app/core/$1',
     '^@shared/(.*)$': '<rootDir>/src/app/shared/$1',
-    '^@env/(.*)$': '<rootDir>/src/environments/$1'
+    '^@env/(.*)$': '<rootDir>/src/environments/$1',
   },
   transform: {
-    '^.+\\.(ts|js|html)$': [
+    '^.+\\.(ts|js|mjs|html|svg)$': [
       'jest-preset-angular',
       {
         tsconfig: '<rootDir>/tsconfig.spec.json',
-        stringifyContentPathRegex: '\\.html$'
-      }
-    ]
+        stringifyContentPathRegex: '\\.html$',
+      },
+    ],
   },
   testMatch: [
     '**/__tests__/**/*.[jt]s?(x)',
-    '**/?(*.)+(spec|test).[jt]s?(x)'
+    '**/?(*.)+(spec|test).[jt]s?(x)',
   ],
-  moduleFileExtensions: ['ts', 'html', 'js', 'json', 'mjs']
+  moduleFileExtensions: ['ts', 'html', 'js', 'json', 'mjs'],
 };
